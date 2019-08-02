8d530a27d1598de2e6ad3f3958d22e82d3fc03be
yin yin <yinyin.khoo21@gmail.com>
2019-08-02 12:16:07 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApplication
+{
+    class Program
+    {
+        enum Days { Sun, Mon, tue, Wed, Thu, Fri, Sat };
+        static void Main(string[] args)
+        {
+
+            Console.Write(Days.Sun);
+
+            Console.ReadKey();
+        }
+    }
+}
